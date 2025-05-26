4a57b2bac05e538f2e97f931d9b76213512179e4
Nikita Kalashnikov <kalaha2006@gmail.com>
2025-05-26 20:28:39 +0300

    fgh

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 tg.disableHeader();
-let button = document.getElementById("close");
-button.addEventListener("click", () => {
-    tg.close
-});
+tg.setHeaderColor('secondary_bg');
 
