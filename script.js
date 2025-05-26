bdaee8e3cf30ec9ef852b85873653eba4197d621
Nikita Kalashnikov <kalaha2006@gmail.com>
2025-05-26 22:59:59 +0300

    change

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let tg = Telegram.WebApp;
-tg.expand();
-tg.hideHeader();cd 
-
+// Инициализация
+if (window.Telegram?.WebApp) {
+  Telegram.WebApp.expand();
+  Telegram.WebApp.setHeaderColor('secondary_bg');
+  
+  // Обработчики кнопок
+  document.getElementById('close-btn').onclick = () => Telegram.WebApp.close();
+}
