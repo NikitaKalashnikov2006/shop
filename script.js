56a22ec395ab12c4a3e83d82c66df3908bd07209
Nikita Kalashnikov <kalaha2006@gmail.com>
2025-06-01 23:50:17 +0300

    ton

--- script.js
+++ script.js
@@ -9,3 +9,43 @@ tg.ready();
 tg.requestFullscreen();
  }
 
+
+
+
+
+
+
+
+
+
+ import { TonConnect } from '@tonconnect/sdk';
+
+const connector = new TonConnect({
+  manifestUrl: 'https://nikitakalashnikov2006.github.io/shop/tonconnect-manifest.json'
+});
+
+// Проверка, подключен ли уже кошелек
+const walletConnectionSource = {
+  jsBridgeKey: 'tonconnect' // Для работы внутри Telegram WebView
+};
+
+async function connectWallet() {
+  const wallets = await connector.getWallets();
+  const wallet = wallets[0]; // Первый доступный кошелек (TonKeeper, OpenMask и др.)
+  
+  const connectionLink = connector.connect(wallet, walletConnectionSource);
+  
+  
+    tg.openLink(connectionLink.universalLink);
+  
+}
+
+// Проверяем, есть ли уже подключенный кошелек
+connector.onStatusChange((wallet) => {
+  if (wallet) {
+    console.log('Кошелек подключен:', wallet);
+  } else {
+    console.log('Кошелек отключен');
+  }
+});
+
