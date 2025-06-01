30ab0b4f4bf1808f248cd3f9dc0ec0820fedabd8
Nikita Kalashnikov <kalaha2006@gmail.com>
2025-06-01 14:25:39 +0300

    hjk

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const tg = window.Telegram.WebApp;
 tg.ready();
-tg.requestFullscreen();
+  
+ function isMobilePhone() {
+  const platform = tg.platform;
+  const userAgent = navigator.userAgent.toLowerCase();
+
+  // Проверяем iOS (iPhone, не iPad)
+  if (platform === "ios") {
+    return !/ipad|tablet/i.test(userAgent); // true только для iPhone
+  }
+
+  // Проверяем Android (телефон, не планшет)
+  if (platform === "android") {
+    return !/tablet/i.test(userAgent); // true только для телефонов
+  }
+
+  // Игнорируем другие платформы (ПК, веб и т.д.)
+  return false;
+}
+
+if (isMobilePhone()) {
+  tg.requestFullscreen(); // Сработает только на iPhone/Android-телефонах
+} else {
+  tg.expand();
+}
+
